8643d4488e57ff0bca844d393b634d38142c8b05
DongWoonKim <01030985843z@naver.com>
2024-08-26 12:04:09 +0900

    ~상속

---
+++ java/java-part-2/src/main/java/com/example/security/G_animal.java
@@ -0,0 +1,19 @@
+package com.example.security;
+
+public class G_animal {
+
+    String name;
+
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+
+    public void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
+
+    public void walk() {
+        System.out.println("Animal is walking.");
+    }
+
+}

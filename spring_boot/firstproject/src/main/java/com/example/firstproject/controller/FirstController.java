8800ded6011e2240d20c9c64cce13b24f69bf67e
DongWoonKim <01030985843z@naver.com>
2024-08-29 12:30:33 +0900

    ~뷰템플릿 페이지 만들기

--- spring_boot/firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ spring_boot/firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FirstController {
 
     @GetMapping("/hi")
-    public String first(Model model) {
+    public String hi(Model model) {
         model.addAttribute("username", "홍길동");
         return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String bye(Model model) {
+        model.addAttribute("nickname", "홍홍홍");
+        return "goodbye";
     }
 
 }

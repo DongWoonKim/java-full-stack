ad6afad103d7cd27b7e5209f43372bac72c39691
DongWoonKim <01030985843z@naver.com>
2024-09-02 10:22:04 +0900

    스프링부트 수업자료 개념편 프로젝트 작업중

---
+++ spring_boot/basic/src/main/java/com/example/basic/domain/Article.java
@@ -0,0 +1,29 @@
+package com.example.basic.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+}

03cdaf42585382a301af34f4550b67585941d18d
DongWoonKim <01030985843z@naver.com>
2024-09-06 12:41:55 +0900

    blog list

---
+++ spring_boot/basic/src/main/java/com/example/basic/dto/ArticleListViewResponse.java
@@ -0,0 +1,19 @@
+package com.example.basic.dto;
+
+import com.example.basic.domain.Article;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}

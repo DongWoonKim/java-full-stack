888294f73ef55182306a380ac05e680d0592bd11
DongWoonKim <01030985843z@naver.com>
2024-09-06 14:16:59 +0900

    blog 상세

--- spring_boot/basic/src/main/java/com/example/basic/controller/BlogViewController.java
+++ spring_boot/basic/src/main/java/com/example/basic/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.example.basic.controller;
 
+import com.example.basic.domain.Article;
 import com.example.basic.dto.ArticleListViewResponse;
+import com.example.basic.dto.ArticleViewResponse;
 import com.example.basic.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -24,6 +27,14 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 
 }

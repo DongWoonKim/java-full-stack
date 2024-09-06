19a663d64464579d2587de28f58f4958db7501ce
DongWoonKim <01030985843z@naver.com>
2024-09-06 15:16:44 +0900

    blog 등록

--- spring_boot/basic/src/main/resources/static/js/article.js
+++ spring_boot/basic/src/main/resources/static/js/article.js
@@ -1,4 +1,28 @@
 
+const createButton = document.getElementById('create-btn');
+
+if (createButton) {
+    createButton.addEventListener('click', event => {
+
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles`, {
+            method: 'POST',
+            headers: {
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify({
+                title : document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        }).then(() => {
+            alert('등록이 완료되었습니다.');
+            location.replace(`/articles`);
+        });
+    });
+}
+
 const modifyButton = document.getElementById('modify-btn');
 
 if (modifyButton) {

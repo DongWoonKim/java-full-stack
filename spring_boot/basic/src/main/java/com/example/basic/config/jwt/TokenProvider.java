a954a60483058539a7fcf7c8504d5cd5857bae88
DongWoonKim <01030985843z@naver.com>
2024-09-11 09:58:26 +0900

    ~security jwt구축

---
+++ spring_boot/basic/src/main/java/com/example/basic/config/jwt/TokenProvider.java
@@ -0,0 +1,87 @@
+package com.example.basic.config.jwt;
+
+import com.example.basic.domain.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class TokenProvider {
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(User user, Duration expiredAt) {
+        Date now = new Date();
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()), user);
+    }
+
+    private String makeToken(Date expiry, User user) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .setSubject(user.getEmail())
+                .claim("id", user.getId())
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret())
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecret())
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new UsernamePasswordAuthenticationToken(
+                new org.springframework.security.core.userdetails.User(claims.getSubject(), "", authorities),
+                token,
+                authorities
+        );
+    }
+
+    public Long getUserId(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("id", Long.class);
+    }
+
+    private Claims getClaims(String token) {
+//        return Jwts.parser()
+//                .setSigningKey(jwtProperties.getSecret())
+//                .parseClaimsJws(token)
+//                .getBody();
+
+//        Key key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes()); // Secret을 Key 객체로 변환
+
+        return Jwts.parserBuilder()
+                .setSigningKey(jwtProperties.getSecret()) // Key 객체 사용
+                .build() // parserBuilder를 통해 빌드
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}

f75ab4f14f30ec88c914fa8adba49ce971f92fc9
DongWoonKim <01030985843z@naver.com>
2024-09-11 23:06:00 +0900

    ~jwt

--- spring_boot/basic/src/main/java/com/example/basic/controller/UserViewController.java
+++ spring_boot/basic/src/main/java/com/example/basic/controller/UserViewController.java
@@ -9,7 +9,7 @@ public class UserViewController {
     @GetMapping("/login")
     public String login() {
         return "login";
-    } 
+    }
 
     @GetMapping("/signup")
     public String signup() {

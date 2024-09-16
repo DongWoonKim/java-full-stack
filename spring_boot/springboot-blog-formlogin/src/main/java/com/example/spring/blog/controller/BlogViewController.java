4cbc4a7def7d92c07cdbba1a1fd72e631df9b473
DongWoonKim <01030985843z@naver.com>
2024-09-16 16:51:45 +0900

    blog view article create/update

--- spring_boot/springboot-blog-formlogin/src/main/java/com/example/spring/blog/controller/BlogViewController.java
+++ spring_boot/springboot-blog-formlogin/src/main/java/com/example/spring/blog/controller/BlogViewController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -30,6 +31,19 @@ public class BlogViewController {
         model.addAttribute("article", article);
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+
+        if (id == null) {
+            model.addAttribute("article", new GetArticleResponse());
+        } else {
+            GetArticleResponse articleResponse = blogService.findById(id);
+            model.addAttribute("article", articleResponse);
+        }
+
+        return "newArticle";
     }
 
 }

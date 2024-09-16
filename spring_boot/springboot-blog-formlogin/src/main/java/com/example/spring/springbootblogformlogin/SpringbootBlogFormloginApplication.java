479bcef1095a77c94215a56dc9e3be6741a85c45
DongWoonKim <01030985843z@naver.com>
2024-09-16 11:35:14 +0900

    blog폼로그인 방식

---
+++ spring_boot/springboot-blog-formlogin/src/main/java/com/example/spring/springbootblogformlogin/SpringbootBlogFormloginApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.springbootblogformlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogFormloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBlogFormloginApplication.class, args);
+    }
+
+}

afe9b9d0dc04e9df7a34633e2230e8bcab528132
DongWoonKim <01030985843z@naver.com>
2024-09-20 15:56:33 +0900

    ~security form login방식 완료

--- spring_boot/springboot-blog-formlogin/src/main/java/com/example/spring/blog/controller/UserApiController.java
+++ spring_boot/springboot-blog-formlogin/src/main/java/com/example/spring/blog/controller/UserApiController.java
@@ -2,8 +2,13 @@ package com.example.spring.blog.controller;
 
 import com.example.spring.blog.dto.AddUserRequest;
 import com.example.spring.blog.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -16,6 +21,12 @@ public class UserApiController {
     public String signUp(AddUserRequest request) {
         userService.save(request);
 
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 
--- spring_boot/springboot-blog-formlogin/src/main/java/com/example/spring/blog/dto/AddUserRequest.java
+++ spring_boot/springboot-blog-formlogin/src/main/java/com/example/spring/blog/dto/AddUserRequest.java
@@ -2,9 +2,13 @@ package com.example.spring.blog.dto;
 
 import com.example.spring.blog.domain.Users;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Getter
+@Setter
+@ToString
 public class AddUserRequest {
     private String email;
     private String password;

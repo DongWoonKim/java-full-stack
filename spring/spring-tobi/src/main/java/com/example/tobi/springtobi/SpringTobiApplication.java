bcd7ba9ef4d67dc347c74edc04faca3c6e8f3ba4
DongWoonKim <01030985843z@naver.com>
2024-09-21 19:00:22 +0900

    ~tobi spring 카테고리 정리

--- spring/spring-tobi/src/main/java/com/example/tobi/springtobi/SpringTobiApplication.java
+++ spring/spring-tobi/src/main/java/com/example/tobi/springtobi/SpringTobiApplication.java
@@ -1,40 +1,8 @@
 package com.example.tobi.springtobi;
 
-import com.example.tobi.springtobi.ex_1_5.dao.CountingConnectionMaker;
-import com.example.tobi.springtobi.ex_1_5.dao.CountingDaoFactory;
-import com.example.tobi.springtobi.ex_1_5.dao.DConnectionMaker;
-import com.example.tobi.springtobi.ex_1_5.dao.UserDao;
-import com.example.tobi.springtobi.ex_1_5.domain.User;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 
 public class SpringTobiApplication {
-
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        setCharacter();
-
-        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
-        dao.get("tobi");
-        dao.get("tobi");
-        dao.get("tobi");
-
-
-        CountingConnectionMaker connectionMaker = context.getBean("connectionMaker", CountingConnectionMaker.class);
-        System.out.println("Conn count : " + connectionMaker.getCounter());
     }
-
-    private static void setCharacter() {
-        try {
-            System.setOut(new PrintStream(System.out, true, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-    }
-
 }

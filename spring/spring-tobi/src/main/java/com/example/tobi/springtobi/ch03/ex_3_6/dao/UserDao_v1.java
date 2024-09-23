7be9fb6858d2ee1c71b524ff9b8a0b8607af2711
DongWoonKim <01030985843z@naver.com>
2024-09-23 17:12:25 +0900

    ~tobi spring ch03_6

---
+++ spring/spring-tobi/src/main/java/com/example/tobi/springtobi/ch03/ex_3_6/dao/UserDao_v1.java
@@ -0,0 +1,77 @@
+package com.example.tobi.springtobi.ch03.ex_3_6.dao;
+
+import com.example.tobi.springtobi.ch03.ex_3_6.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserDao_v1 {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public UserDao_v1(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void add(User user) {
+        this.jdbcTemplate.update(
+                "INSERT INTO users(id, name, password) VALUES(?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword()
+        );
+    }
+
+    public List<User> getAll() {
+        String sql = "SELECT * FROM users";
+
+        return jdbcTemplate.query(sql, new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+
+                return null;
+            }
+        });
+    }
+
+    public User get(String id) {
+        String sql = "SELECT * FROM users WHERE id = ?";
+
+        return jdbcTemplate.queryForObject(sql, new Object[]{id}, new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
+        });
+    }
+
+    public int getCount() {
+        String sql = "SELECT COUNT(*) FROM users";
+        return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
+
+    public void deleteAll() {
+        this.jdbcTemplate.update(
+                new PreparedStatementCreator() {
+                    @Override
+                    public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                        return con.prepareStatement("DELETE FROM users");
+                    }
+                }
+        );
+    }
+
+}

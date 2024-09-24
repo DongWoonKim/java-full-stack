041c2a594ea14eae25d76cddc13d716e09115772
DongWoonKim <01030985843z@naver.com>
2024-09-24 21:41:17 +0900

    ~tobi spring ch05_2

--- spring/spring-tobi/src/main/java/com/example/tobi/springtobi/ch05/ex_5_2/service/UserService.java
+++ spring/spring-tobi/src/main/java/com/example/tobi/springtobi/ch05/ex_5_2/service/UserService.java
@@ -3,7 +3,12 @@ package com.example.tobi.springtobi.ch05.ex_5_2.service;
 import com.example.tobi.springtobi.ch05.ex_5_2.dao.UserDao;
 import com.example.tobi.springtobi.ch05.ex_5_2.domain.Level;
 import com.example.tobi.springtobi.ch05.ex_5_2.domain.User;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserService {
@@ -12,6 +17,21 @@ public class UserService {
     public static final int MIN_LOGCOUNT_FOR_GOLD = 30;
 
     private UserDao userDao;
+    private DataSource dataSource;
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public UserService(String id) {}
 
     public UserService(UserDao userDao) {
         this.userDao = userDao;
@@ -32,6 +52,31 @@ public class UserService {
 
     }
 
+    public void upgradeLevelsV2() throws SQLException {
+
+        TransactionSynchronizationManager.initSynchronization();
+        Connection c = DataSourceUtils.getConnection(dataSource);
+        c.setAutoCommit(false);
+
+        try {
+            List<User> users = userDao.getAll();
+
+            for(User user : users) {
+                if (canUpgradeLevel(user)) upgradeLevel(user);
+            }
+
+            c.commit();
+        } catch (Exception e) {
+            c.rollback();
+            throw e;
+        } finally {
+            DataSourceUtils.releaseConnection(c, dataSource);
+            TransactionSynchronizationManager.unbindResource(this.dataSource);
+            TransactionSynchronizationManager.clearSynchronization();
+        }
+
+    }
+
     private boolean canUpgradeLevel(User user) {
         Level currentLevel = user.getLevel();
         switch (currentLevel) {
@@ -42,9 +87,32 @@ public class UserService {
         }
     }
 
-    private void upgradeLevel(User user) {
+    protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
     }
 
+    static class TestUserService extends UserService {
+
+        private String id;
+
+        public TestUserService(String id) {
+            super(id);
+            this.id = id;
+        }
+
+        @Override
+        protected void upgradeLevel(User user) {
+            if ( user.getId().equals(this.id) )
+                throw new TestUserServiceException();
+            super.upgradeLevel(user);
+        }
+    }
+
+    static class TestUserServiceException extends RuntimeException {
+
+    }
+
 }
+
+
---
+++ spring/spring-tobi/src/test/java/com/example/tobi/springtobi/ch05/ex_5_2/service/UserServiceTest.java
@@ -0,0 +1,82 @@
+package com.example.tobi.springtobi.ch05.ex_5_2.service;
+
+import com.example.tobi.springtobi.ch05.ex_5_2.domain.Level;
+import com.example.tobi.springtobi.ch05.ex_5_2.dao.DaoFactory;
+import com.example.tobi.springtobi.ch05.ex_5_2.dao.UserDao;
+import com.example.tobi.springtobi.ch05.ex_5_2.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = DaoFactory.class)
+class UserServiceTest {
+
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private DataSource dataSource;
+    List<User> users;
+
+    @BeforeEach
+    public void setUp() {
+        userDao.deleteAll();
+        users = Arrays.asList(
+                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, 29),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+                new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    void upgradeAllOrNothing() {
+        UserService testUserService = new UserService.TestUserService( users.get(3).getId() );
+        testUserService.setUserDao(this.userDao);
+
+        userDao.deleteAll();
+
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        System.out.println(userDao.get("joytouch").getLevel());
+        try {
+            testUserService.upgradeLevels();
+        } catch (Exception e) {}
+
+
+        System.out.println(userDao.get("joytouch").getLevel());
+    }
+
+    @Test
+    void upgradeAllOrNothing_v2() {
+        UserService testUserService = new UserService.TestUserService( users.get(3).getId() );
+        testUserService.setUserDao(this.userDao);
+        testUserService.setDataSource(dataSource);
+
+        userDao.deleteAll();
+
+        for (User user : users) {
+            userDao.add(user);
+        }
+
+        System.out.println(userDao.get("joytouch").getLevel());
+        try {
+            testUserService.upgradeLevelsV2();
+        } catch (Exception e) {}
+
+
+        System.out.println(userDao.get("joytouch").getLevel());
+    }
+}

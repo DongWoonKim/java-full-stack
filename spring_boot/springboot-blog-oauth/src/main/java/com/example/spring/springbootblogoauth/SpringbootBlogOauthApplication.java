6b647ff6cd47c423283f12e92cf8d9d8b367d1ec
DongWoonKim <01030985843z@naver.com>
2024-09-26 16:07:18 +0900

    blog oauth setup

---
+++ spring_boot/springboot-blog-oauth/src/main/java/com/example/spring/springbootblogoauth/SpringbootBlogOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.springbootblogoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBlogOauthApplication.class, args);
+    }
+
+}
---
+++ spring_boot/springboot-blog-oauth/src/test/java/com/example/spring/springbootblogoauth/SpringbootBlogOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.springbootblogoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

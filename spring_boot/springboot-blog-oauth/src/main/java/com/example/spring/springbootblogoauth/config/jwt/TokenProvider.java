79d6c3f6f8268ea146dcc2e8acd432e2091ac10f
DongWoonKim <01030985843z@naver.com>
2024-09-30 15:18:23 +0900

    prj token provider 생성완료

---
+++ spring_boot/springboot-blog-oauth/src/main/java/com/example/spring/springbootblogoauth/config/jwt/TokenProvider.java
@@ -0,0 +1,99 @@
+package com.example.spring.springbootblogoauth.config.jwt;
+
+import com.example.spring.springbootblogoauth.domain.Users;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.Duration;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class TokenProvider {
+
+    private JwtProperties jwtProperties;
+
+    public String generateToken(Users user, Duration expiredAt) {
+        Date now = new Date();
+        return makeToken(
+                new Date(now.getTime() + expiredAt.toMillis()),
+                user
+        );
+    }
+
+    public boolean validToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey( getSecretKey() ) // SecretKey 객체 사용
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return true;
+        } catch (Exception e) {
+            // 복호화 과정에서 에러가 나면 유효하지 않은 토큰
+            return false;
+        }
+    }
+
+    // 토큰 기반으로 인증 정보를 가져오는 메서드
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(
+                new SimpleGrantedAuthority("ROLE_USER")
+        );
+
+        return new UsernamePasswordAuthenticationToken(
+                new User(claims.getSubject(), "", authorities),
+                token,
+                authorities
+        );
+    }
+
+    // 토큰 기반으로 user id를 가져오는 메서드
+    public Long getUserId(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("id", Long.class);
+    }
+
+    private String makeToken(Date expire, Users user) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuedAt(now)
+                .setExpiration(expire)
+                .setSubject(user.getEmail())
+                .claim("id", user.getId())
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.getDecoder().decode(jwtProperties.getSecretKey());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())  // SecretKey 객체 사용
+                .build()                    // 파서 빌드
+                .parseClaimsJws(token)      // 토큰 파싱
+                .getBody();                 // 클레임 추출
+    }
+
+}

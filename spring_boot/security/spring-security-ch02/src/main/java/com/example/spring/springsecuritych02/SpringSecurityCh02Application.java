3aef57a4a133bbf8bfd813ee5328264c28affec2
DongWoonKim <01030985843z@naver.com>
2024-09-30 15:52:58 +0900

    springboot security ch02 set up

---
+++ spring_boot/security/spring-security-ch02/src/main/java/com/example/spring/springsecuritych02/SpringSecurityCh02Application.java
@@ -0,0 +1,13 @@
+package com.example.spring.springsecuritych02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCh02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityCh02Application.class, args);
+    }
+
+}
---
+++ spring_boot/security/spring-security-ch02/src/test/java/com/example/spring/springsecuritych02/SpringSecurityCh02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.springsecuritych02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCh02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

5b14bfb42e4414945cd2a593122b45572c3c916a
DongWoonKim <01030985843z@naver.com>
2024-10-05 23:45:26 +0900

    spring boot board form방식 회원가입/로그인 구현

--- spring_boot/springboot-basic-board/src/main/java/com/example/tobi/springbootbasicboard/config/security/CustomAuthenticationFailureHandler.java
+++ spring_boot/springboot-basic-board/src/main/java/com/example/tobi/springbootbasicboard/config/security/CustomAuthenticationFailureHandler.java
@@ -4,6 +4,7 @@ import com.example.tobi.springbootbasicboard.dto.SignInResponseDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -11,9 +12,10 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
--- spring_boot/springboot-basic-board/src/main/java/com/example/tobi/springbootbasicboard/config/security/CustomAuthenticationSuccessHandler.java
+++ spring_boot/springboot-basic-board/src/main/java/com/example/tobi/springbootbasicboard/config/security/CustomAuthenticationSuccessHandler.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -14,9 +15,10 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {

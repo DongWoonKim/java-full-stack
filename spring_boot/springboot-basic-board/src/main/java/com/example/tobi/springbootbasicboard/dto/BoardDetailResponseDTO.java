440f10ba0ddd0a22c5255869f3bc827346c8f3f1
DongWoonKim <01030985843z@naver.com>
2024-10-05 11:20:03 +0900

    springboot basic board

---
+++ spring_boot/springboot-basic-board/src/main/java/com/example/tobi/springbootbasicboard/dto/BoardDetailResponseDTO.java
@@ -0,0 +1,15 @@
+package com.example.tobi.springbootbasicboard.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class BoardDetailResponseDTO {
+    private String title;
+    private String content;
+    private String userId;
+    private LocalDateTime created;
+}
